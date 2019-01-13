c8740cb660dd42e0cb1ff98001a4ef5611d6ddb5
Akash Hardia <aakashhardiya@gmail.com>
2019-01-13 12:19:39 +0000

    feat-Create array of work items
    Contains objects containing title, preview, img, color for every work

---
+++ js/app.js
@@ -0,0 +1,38 @@
+const arr = [
+        {
+            title: "Concentration",
+            href: "https://htmlpreview.github.io/?https://github.com/akashhardia/memory-game-Concentration/blob/master/index.html",
+            img: "background-image: url(img/concentration.png)",
+            color: "background-color: rgba(125,151,173,.8)"
+        },
+        {
+            title: "Pixel Art Maker",
+            href: "https://htmlpreview.github.io/?https://github.com/akashhardia/pixel-art-maker/blob/master/index.html",
+            img: "background-image: url(img/pixel_nexus.jpeg)",
+            color: "background-color: rgba(255,162,52,.8)"
+        },
+        {
+            title: "Portfolio",
+            href: "#",
+            img: "background-image: url(img/portfolio_nexus.jpeg)",
+            color: "background-color: rgba(169,81,237,.8)"
+        },
+        {
+            title: "Notes Taker",
+            href: "https://htmlpreview.github.io/?https://github.com/akashhardia/notes_taker/blob/master/index.html",
+            img: "background-image: url(img/notes_nexus.jpeg)",
+            color: "background-color: rgba(2,179,228,.8)"
+        },
+        {
+            title: "Animal Trading Card",
+            href: "http://htmlpreview.github.io/?https://github.com/akashhardia/udacity/blob/master/animal%20trading%20card/card.html",
+            img: "background-image: url(img/animal_nexus.jpeg)",
+            color: "background-color: rgba(2,204,186,.8)"
+        },
+        {
+            title: "Classic Arcade Game",
+            href: "https://htmlpreview.github.io/?https://github.com/akashhardia/Classic-Arcade-Game-Clone/blob/master/index.html",
+            img: "background-image: url(img/Classic_arcade.png)",
+            color: "background-color: rgba(255,84,131,.8)"
+        }
+    ];
